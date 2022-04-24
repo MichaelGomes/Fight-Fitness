efea8f688f3a775114eead4a8168c5f7ab526d02
Michael Gomes <michaelgomesweb@gmail.com>
2022-04-24 18:39:20 -0400

    Add Procfile & Modify Port Listen For Heroku

--- app.js
+++ app.js
@@ -63,6 +63,6 @@ app.post("/newsletter", function (req, res) {
 });
 
 //Listen
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000");
 });
